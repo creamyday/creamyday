14cb9443c3663402a4778dd534aaa026a05342b7
alice <aalice851215@gmail.com>
2026-01-11 19:05:36 +0800

    Chore:更新路由

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router";
 // custom page
 import App from "./App";
 import Login from "./pages/Login";
 import Home from "./pages/admin/Home";
 import About from "./pages/admin/About";
 
-const createRoutes = createBrowserRouter([
+const createRoutes = createHashRouter([
   {
     path: "/",
     Component: App,
