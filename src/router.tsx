09c7ff90111c637f967652b900e78090352e3b4a
鄭舜仁 <killer80548@gmail.com>
2026-01-18 13:46:12 +0800

    chore:變更router內import路徑

--- src/router.tsx
+++ src/router.tsx
@@ -2,8 +2,8 @@ import { createHashRouter } from "react-router";
 // custom page
 import App from "./App";
 import Login from "./pages/Login";
-import Home from "./pages/admin/Home";
-import About from "./pages/admin/About";
+import Home from "./pages/customer/Home";
+import About from "./pages/customer/About";
 
 const createRoutes = createHashRouter([
   {
