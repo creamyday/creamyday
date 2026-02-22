79233a47781ffb9e6b27e83e91937d00f732980e
irtsey88 <irtsey@gmail.com>
2026-02-22 15:52:29 +0800

    暫存修改

--- src/pages/customer/Home.tsx
+++ src/pages/customer/Home.tsx
@@ -1,18 +1,76 @@
-import '../../assets/pages/customer/home.scss';
+import "../../assets/pages/customer/home.scss";
+import { Link } from "react-router";
 
-export default function Home() {
+const slides = [
+  {
+    image: "/hero1.png",
+    title: "CreamyDay",
+    desc: "為平凡的日子，添上一點甜",
+    buttonText: "立即選購 →",
+    link: "/products/cake/1",
+  },
+  {
+    image: "/hero2.png",
+    title: "CreamyDay",
+    desc: "黑糖珍珠奶茶起司蛋糕",
+    buttonText: "立即查看 →",
+    link: "/new",
+  },
+  {
+    image: "/hero3.jpg",
+    title: "CreamyDay",
+    desc: "聖誕限定！派對蛋糕限定登場",
+    buttonText: "最新消息 →",
+    link: "/hot",
+  },
+];
+
+export default function HeroCarousel() {
   return (
     <div className="hero-wrapper">
+      <div className="container">
+        <div
+          id="carouselExample"
+          className="carousel slide hero-carousel"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-indicators">
+            {slides.map((_, index) => (
+              <button
+                key={index}
+                type="button"
+                data-bs-target="#carouselExample"
+                data-bs-slide-to={index}
+                className={index === 0 ? "active" : ""}
+              />
+            ))}
+          </div>
 
-      {/* 背景圖 */}
-      <img
-        src="./public/carousel_hero_bg.png"
-        className="hero-bg"
-        alt="background"
-      />
-      
+          <div className="carousel-inner">
+            {slides.map((slide, index) => (
+              <div
+                key={index}
+                className={`carousel-item ${index === 0 ? "active" : ""}`}
+              >
+                <img src={slide.image} alt={slide.title} />
 
-      
+                <div className="carousel-caption custom-caption">
+                  <div className="caption-content">
+                    <div className="caption-text">
+                      <h2>{slide.title}</h2>
+                      <p>{slide.desc}</p>
+                    </div>
+
+                    <Link to={slide.link} className="hero-btn">
+                      {slide.buttonText}
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
